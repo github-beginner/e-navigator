015a884f5b548537339d248bd1a372483fc443dd
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-02-23 14:47:26 +0000

    Permit parameter "name" in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :sex, :school, birthday: [:year, :month, :day]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :school, birthday: [:year, :month, :day]])
   end
 end
