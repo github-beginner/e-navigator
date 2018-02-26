f280f3ae8150edf845337eca2af98caa53a56b42
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-02-26 09:37:59 +0000

    Permit paramaters in edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :school, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :sex, :school, :birthday])
   end
 end
