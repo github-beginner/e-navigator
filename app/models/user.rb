c55133ea3b12b9818d48399898392272d4f4b77e
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-02-26 11:42:10 +0000

    Define sex in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  enum sex:{man:0,woman:1}
 end
