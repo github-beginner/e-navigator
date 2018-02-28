51da8d527dcd3d87a8879adb56e87cbafe9232ac
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-02-28 09:52:26 +0000

    Set users to users#indes as user_root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'users', :to => 'users#index', :as => :user_root
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
