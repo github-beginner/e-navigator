cc787c5a11256ecbb6f7c7bf1f54b24421674c75
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-03-01 15:22:26 +0000

    Use age method to prevent calculating in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,5 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @users.each do |user|
-      d1 = user.birthday.strftime("%Y%m%d").to_i
-      d2 = Date.today.strftime("%Y%m%d").to_i
-      user.birthday = (d2 - d1) / 10000
-    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum sex: { man:0, woman:1 }
+  def age
+    d1 = self.birthday.strftime("%Y%m%d").to_i
+    d2 = Date.today.strftime("%Y%m%d").to_i
+    (d2 - d1) / 10000
+  end
 end
