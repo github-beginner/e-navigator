133735d6a87b80b730171344e095e6e1b03cfffb
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-03-06 14:52:06 +0000

    Edit intervoew_controller.rb to check authentication

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,4 +1,5 @@
 class InterviewsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_interview, only: [:show, :edit, :update, :destroy]
 
   # GET /interviews
