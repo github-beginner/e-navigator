0e1aaa2cc9f626b5fc1be7fd5c11723eaf3adc3c
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-03-06 14:02:40 +0000

    Set enum for availavility

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,4 @@
 class Interview < ApplicationRecord
   belongs_to :user
+  enum availability: { accept:0, reject:1, reservation:3 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306043433) do
+ActiveRecord::Schema.define(version: 20180306133106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "interviews", force: :cascade do |t|
     t.datetime "date"
-    t.integer "availability"
+    t.integer "availability", default: 3
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
