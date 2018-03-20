6a782d9b20a1e345ac7b9d0eb6f7b26bcb23704e
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-03-20 02:17:39 +0000

    Fix html method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'users', :to => 'users#index', :as => :user_root
   root :to => 'users#index'
   devise_for :users
-  get 'send_request_email', :to => 'interviews#send_request_email'
+  post 'send_request_email', :to => 'interviews#send_request_email'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
