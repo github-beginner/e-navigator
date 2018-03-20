07ecd2e3b39b3c71c230694afc8680513553357e
github-beginner <36471934+github-beginner@users.noreply.github.com>
2018-03-20 03:18:07 +0000

    Fix bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,7 +11,7 @@ class UserMailer < ApplicationMailer
   def decision_email_for_interviewer(request_user, interviewer)
     @request_user = request_user
     @interviewer = interviewer
-    @url = ENV['url']
+    @url = ENV['URL']
     mail(to: @interviewer.email, subject: '面接の日程が承認されました')
   end
 
